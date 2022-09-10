63523a225ddd8d83f8297d1fc7f0621530a64fb5
Nazar <n.dusyak@avto.pro>
2022-09-10 16:07:07 +0300

    added sourse code

---
+++ src/com/company/maths.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class maths {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int h = 8;
+
+        int volume = a*b*h;
+        int length = 4*(a+b+h);
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Cумарна довжина всіх сторін паралелепіпеда = " + length);
+    }
+
+
+}
---
+++ src/com/company/position.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class position{
+
+        public static void main(String[] args) {
+
+            String name ="Координаты Эйфелевой башни";
+            double N = 48.858276;
+            double E = 2.294591 ;
+
+
+            System.out.println(name+":");
+            System.out.println("N = "+N+"°;");
+            System.out.println("E = "+E+"°.");
+
+
+        }
+}
